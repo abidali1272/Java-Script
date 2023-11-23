4df389db4f1577633d6d0d1d2390233fa4e3d74c
abidali1272 <kabid7apr@gmail.com>
2023-11-23 12:49:50 +0530

    script learning

---
+++ start.js
@@ -0,0 +1,39 @@
+//document.write("hello world in java scriopt");  
+//document.write("hello world in java scriopt"); 
+
+// after variable all code is here 
+
+let lastname="khan sahbbir ali";
+document.write(lastname+"<br>");
+
+// constant variable
+let a="a";
+var b=12.33;
+const c="password";
+
+// Data type in java
+
+var b="center is greate"; //String
+ let bb={abid:"khan",shabbir:"shabbir"}; // number
+var b=["khan","shaikh","choudhry"];  //array
+var b={first:"jan",last:"dec"}; // object
+var b=null; // null
+var b=false; //boolean
+
+document.write(typeof b);
+
+// all console command is here 
+
+console.time("test");
+console.log(bb);
+console.table(bb);
+console.error("something went rong");
+console.timeEnd("test");
+
+
+
+
+
+
+
+
